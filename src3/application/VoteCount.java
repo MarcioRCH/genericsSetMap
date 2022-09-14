ffa37f640b30c5543acb79952f390779541ff625
MarcioRCH <marciorchsilva@gmail.com>
2022-09-14 16:15:04 -0300

    Map update

--- src3/application/VoteCount.java
+++ src3/application/VoteCount.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class VoteCount {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader (path))){
+			
+		}
+		catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();
 	}
 
 }
